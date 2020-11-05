353cbeca340a9adb53e8e04ccaa6d4ce7e09f542
Mikhail Puzanov <misha.puzanov@gmail.com>
2020-11-05 13:08:26 +0100

    Make file extension check case-insensitive.

--- src/RPKI/Parse/Parse.hs
+++ src/RPKI/Parse/Parse.hs
@@ -10,23 +10,24 @@ module RPKI.Parse.Parse (
 )
 where
 
-import RPKI.Parse.Internal.Common
-import RPKI.Parse.Internal.Cert
-import RPKI.Parse.Internal.CRL
-import RPKI.Parse.Internal.MFT
-import RPKI.Parse.Internal.ROA
-import RPKI.Parse.Internal.SignedObject
+import qualified Data.ByteString                  as BS
+import           Data.Char                        (toLower)
+import qualified Data.List                        as List
+import qualified Data.Text                        as Text
 
-import RPKI.Domain
+import           RPKI.Parse.Internal.Cert
+import           RPKI.Parse.Internal.Common
+import           RPKI.Parse.Internal.CRL
+import           RPKI.Parse.Internal.MFT
+import           RPKI.Parse.Internal.ROA
+import           RPKI.Parse.Internal.SignedObject
 
-import qualified Data.ByteString as BS
-import qualified Data.List as List
-import qualified Data.Text as Text
+import           RPKI.Domain
 
 -- | 
 supportedExtension :: String -> Bool
 supportedExtension filename = 
-    let ext = List.drop (List.length filename - 4) filename
+    let ext = map toLower $ List.drop (List.length filename - 4) filename
         in elem ext [".cer", ".mft", ".crl", ".roa", ".gbr"] 
 
 -- | Parse object from a bytesting containing ASN1 representaton
@@ -34,15 +35,14 @@ supportedExtension filename =
 readObject :: RpkiURL -> BS.ByteString -> ParseResult RpkiObject
 readObject objectURL content = do    
     let URI u = getURL objectURL
-    let ext = Text.unpack $ Text.drop (Text.length u - 3) u
+    let ext = map toLower $ Text.unpack $ Text.drop (Text.length u - 4) u
     case ext of
-        "cer" -> parse_ objectURL parseResourceCertificate CerRO content            
-        "mft" -> parse_ objectURL parseMft MftRO content
-        "roa" -> parse_ objectURL parseRoa RoaRO content            
-        "crl" -> parse_ objectURL parseCrl CrlRO content            
-        _     -> Left $ fmtErr $ "Unknown object type: " <> show u
+        ".cer" -> parse_ objectURL parseResourceCertificate CerRO content            
+        ".mft" -> parse_ objectURL parseMft MftRO content
+        ".roa" -> parse_ objectURL parseRoa RoaRO content            
+        ".crl" -> parse_ objectURL parseCrl CrlRO content            
+        _      -> Left $ fmtErr $ "Unknown object type: " <> show u
         where
             parse_ u parse constructor bs = do
                 f <- parse bs
                 pure $ constructor (f u)
-
