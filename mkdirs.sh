8d929528b730bd1361eeef98cbd721d569138a4a
Misha Puzanov <misha.puzanov@gmail.com>
2020-12-27 16:08:13 +0100

    Allow for arbitrary directories

--- mkdirs.sh
+++ mkdirs.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-mkdir -p ${HOME}/.rpki/cache
-mkdir -p ${HOME}/.rpki/tmp
-mkdir -p ${HOME}/.rpki/rsync
-mkdir -p ${HOME}/.rpki/tals
+ROOT=$1
 
-cd ${HOME}/.rpki/tals
+mkdir -p ${ROOT}/cache
+mkdir -p ${ROOT}/tmp
+mkdir -p ${ROOT}/rsync
+mkdir -p ${ROOT}/tals
 
-wget https://raw.githubusercontent.com/NLnetLabs/routinator/master/tals/afrinic.tal
-wget https://raw.githubusercontent.com/NLnetLabs/routinator/master/tals/apnic.tal
-wget https://raw.githubusercontent.com/NLnetLabs/routinator/master/tals/arin.tal
-wget https://raw.githubusercontent.com/NLnetLabs/routinator/master/tals/lacnic.tal
-wget https://raw.githubusercontent.com/NLnetLabs/routinator/master/tals/ripe.tal
+curl -s https://raw.githubusercontent.com/NLnetLabs/routinator/master/tals/afrinic.tal > ${ROOT}/tals/afrinic.tal
+curl -s https://raw.githubusercontent.com/NLnetLabs/routinator/master/tals/apnic.tal > ${ROOT}/tals/apnic.tal
+curl -s https://raw.githubusercontent.com/NLnetLabs/routinator/master/tals/arin.tal > ${ROOT}/tals/arin.tal
+curl -s https://raw.githubusercontent.com/NLnetLabs/routinator/master/tals/lacnic.tal > ${ROOT}/tals/lacnic.tal
+curl -s https://raw.githubusercontent.com/NLnetLabs/routinator/master/tals/ripe.tal > ${ROOT}/tals/ripe.tal
