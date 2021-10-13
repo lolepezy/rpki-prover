b67589dcbfc4cb3dd84fcfab86a9018870557ad4
Misha Puzanov <misha.puzanov@gmail.com>
2021-10-13 23:35:19 +0200

    Fix test compilation

--- test/src/RPKI/Orphans.hs
+++ test/src/RPKI/Orphans.hs
@@ -431,6 +431,10 @@ instance Arbitrary InitError where
     arbitrary = genericArbitrary
     shrink = genericShrink
 
+instance Arbitrary SlurmError where
+    arbitrary = genericArbitrary
+    shrink = genericShrink
+
 instance Arbitrary AppError where
     arbitrary = genericArbitrary
     shrink = genericShrink
