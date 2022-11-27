d75f12bbe8caf903b3eb4cf60ae695ebcedc7f74
Misha Puzanov <misha.puzanov@gmail.com>
2022-11-27 19:23:35 +0100

    Fix tests.

--- test/src/RPKI/Orphans.hs
+++ test/src/RPKI/Orphans.hs
@@ -523,6 +523,10 @@ instance Arbitrary RsyncMetric where
 instance Arbitrary RrdpMetric where
     arbitrary = genericArbitrary
     shrink = genericShrink
+
+instance Arbitrary InternalMetric where
+    arbitrary = genericArbitrary
+    shrink = genericShrink
     
 instance Arbitrary VrpCounts where
     arbitrary = genericArbitrary
@@ -550,6 +554,10 @@ generateMap constructor = do
     pure $ constructor $ Map.fromList validations
 
 instance Arbitrary TimeMs where
+    arbitrary = genericArbitrary
+    shrink = genericShrink
+
+instance Arbitrary CPUTime where
     arbitrary = genericArbitrary
     shrink = genericShrink
 
