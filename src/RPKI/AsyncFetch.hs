5746da0880d479a46ba1edbc32c19a0117f6b85d
Misha Puzanov <misha.puzanov@gmail.com>
2023-06-12 13:15:26 +0200

    Add empty AsyncFetch

---
+++ src/RPKI/AsyncFetch.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE DerivingStrategies   #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedLabels     #-}
+{-# LANGUAGE QuasiQuotes          #-}
+{-# LANGUAGE RecordWildCards      #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE OverloadedStrings    #-}
+
+module RPKI.AsyncFetch where
+
+import           Control.Concurrent.Async
+import           Control.Concurrent
+import           Control.Concurrent.STM
+import           Control.Exception
+import           Control.Monad
+
+import           Control.Lens
+import           Data.Generics.Product.Typed
+import           GHC.Generics (Generic)
+
+import           Data.List
+import           Data.Foldable (for_)
+import           Data.Int                         (Int64)
+import qualified Data.Text                        as Text
+import qualified Data.Map.Strict as Map
+import           Data.Maybe                       (fromMaybe)
+
+import           Data.Hourglass
+import           Data.String.Interpolate.IsString
+import           System.Exit
+import           System.Directory
+import           System.FilePath                  ((</>))
+
+import           RPKI.AppState
+import           RPKI.AppMonad
+import           RPKI.AppTypes
+import           RPKI.Config
+import           RPKI.Domain
+import           RPKI.Messages
+import           RPKI.Reporting
+import           RPKI.Logging
+import           RPKI.Metrics.System
+import           RPKI.Parallel
+import           RPKI.Store.Database
+import           RPKI.Validation.TopDown
+
+import           RPKI.AppContext
+import           RPKI.Metrics.Prometheus
+import           RPKI.RTR.RtrServer
+import           RPKI.Store.Base.Storage
+import           RPKI.TAL
+import           RPKI.Time
+import           RPKI.Worker
+
+import           RPKI.Store.AppStorage
+import           RPKI.SLURM.Types
