984f76aed7363c27c0a31262bee4a954dfbd9b9e
Misha Puzanov <misha.puzanov@gmail.com>
2023-07-23 01:31:33 +0200

    cleanup

--- scripts/build-static.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd ..
-
-docker build - < Dockerfile.static-builder --tag rpki-prover-builder
-stack install --docker --docker-image "rpki-prover-builder" --no-nix rpki-prover:rpki-prover-static
