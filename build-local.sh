4d1ef1d3394aa3ca620d24155816b38e2f841331
Mikhail Puzanov <misha.puzanov@gmail.com>
2023-08-18 00:38:54 +0200

    Fix build on apple silicon+brew

--- build-local.sh
+++ build-local.sh
@@ -1,2 +1,5 @@
 cp package-template.yaml package.yaml
-stack install rpki-prover:exe:rpki-prover --stack-yaml=stack-9.6.yaml
+if [[ $(uname -m) == 'arm64' ]]; then
+    EXTRA_LIBRARIES="--extra-include-dirs=/opt/homebrew/include --extra-lib-dirs=/opt/homebrew/lib"
+fi
+stack install rpki-prover:exe:rpki-prover --stack-yaml=stack-9.6.yaml ${EXTRA_LIBRARIES}
