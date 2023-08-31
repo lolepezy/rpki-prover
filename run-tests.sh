9028658d6ac8c3a9262130f7031ad6d8de4bbc57
Mikhail Puzanov <misha.puzanov@gmail.com>
2023-08-31 09:46:31 +0200

    8.10.7 doesn't work on apple silicon

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,8 @@
 cp package-template.yaml package.yaml
 if [[ $(uname -m) == 'arm64' ]]; then
     EXTRA_LIBRARIES="--extra-include-dirs=/opt/homebrew/include --extra-lib-dirs=/opt/homebrew/lib"
+    stack test rpki-prover:lib rpki-prover:test:rpki-prover-test --stack-yaml=stack-9.6.yaml ${EXTRA_LIBRARIES}
+else 
+    stack test rpki-prover:lib rpki-prover:test:rpki-prover-test --stack-yaml=stack-9.6.yaml && \
+    stack test rpki-prover:lib rpki-prover:test:rpki-prover-test --stack-yaml=stack-8.10.yaml
 fi
-stack test rpki-prover:lib rpki-prover:test:rpki-prover-test --stack-yaml=stack-9.6.yaml ${EXTRA_LIBRARIES} && \
-stack test rpki-prover:lib rpki-prover:test:rpki-prover-test --stack-yaml=stack-8.10.yaml ${EXTRA_LIBRARIES}
