181de3f5ad65fd52fc61d2a918ef9f25bd8798ee
Mikhail Puzanov <misha.puzanov@gmail.com>
2024-10-01 19:03:49 +0200

    Fix compressing maps

--- test/src/RPKI/Store/DiffSpec.hs
+++ test/src/RPKI/Store/DiffSpec.hs
@@ -5,6 +5,7 @@
 module RPKI.Store.DiffSpec where
 
 import           Data.Set                 (Set)
+import qualified Data.Set                 as Set
 import           Data.Map.Strict          (Map)
 import qualified Data.Map.Strict          as Map
 import qualified Data.Map.Monoidal.Strict as MonoidalMap
@@ -19,8 +20,10 @@ import           RPKI.Reporting
 import           RPKI.Domain
 import           RPKI.Store.Diff
 
+
+
 diffGroup :: TestTree
-diffGroup = testGroup "PublicationPoints" [
+diffGroup = localOption (QC.QuickCheckTests 300) $ testGroup "Diffs" [        
         QC.testProperty "Aspa applyDiff works" $ checkDiff @(Set Aspa),
         QC.testProperty "SplN applyDiff works" $ checkDiff @(Set SplN),
         QC.testProperty "Gbr applyDiff works" $ checkDiff @(Set (T2 Hash Gbr)),
@@ -38,39 +41,43 @@ diffGroup = testGroup "PublicationPoints" [
     ]
 
 
-checkDiff :: (Eq a, Shrinkable a, Diffable a) => a -> a -> Bool
+checkDiff :: (Eq a, Compressable a, Diffable a) => a -> a -> Bool
 checkDiff before after = 
     if (applyDiff before (makeDiff before after) == after)
         then True
         else let 
+            -- The only reasons tests are not allowed to pass is
+            -- empty map values that will be filtered out by the diff 
+            -- procedure.
             bShrunk = shrink before
             aShrunk = shrink after
         in applyDiff bShrunk (makeDiff bShrunk aShrunk) == aShrunk        
 
-class Shrinkable a where 
+class Compressable a where 
     shrink :: a -> a     
     shrink = id
     isEmpty :: a -> Bool
     isEmpty _ = False
 
-instance Shrinkable (Set v)
+instance Compressable (Set v) where    
+    isEmpty = Set.null
 
-instance Shrinkable Roas where    
+instance Compressable Roas where    
     shrink = Roas . shrink . unRoas
     isEmpty = isEmpty . unRoas
 
-instance Shrinkable Vrps where    
+instance Compressable Vrps where    
     shrink = Vrps . shrink . unVrps
     isEmpty = isEmpty . unVrps
 
-instance Shrinkable Validations where    
+instance Compressable Validations where    
     shrink = Validations . shrink . unValidations
     isEmpty = isEmpty . unValidations
 
-instance Shrinkable v => Shrinkable (Map k v) where    
+instance Compressable v => Compressable (Map k v) where    
     shrink = Map.filter (not . isEmpty) . Map.map shrink 
     isEmpty = Map.null
 
-instance Shrinkable v => Shrinkable (MonoidalMap.MonoidalMap k v) where    
+instance Compressable v => Compressable (MonoidalMap.MonoidalMap k v) where    
     shrink = MonoidalMap.filter (not . isEmpty) . MonoidalMap.map shrink 
     isEmpty = MonoidalMap.null
