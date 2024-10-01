c115da2992d559def4e49b49d06b5e441d0f3a7e
Mikhail Puzanov <misha.puzanov@gmail.com>
2024-10-01 18:35:15 +0200

    Fix test for all cases

--- test/src/RPKI/Store/DiffSpec.hs
+++ test/src/RPKI/Store/DiffSpec.hs
@@ -5,7 +5,7 @@
 module RPKI.Store.DiffSpec where
 
 import           Data.Set                 (Set)
-import qualified Data.Set                 as Set
+import           Data.Map.Strict          (Map)
 import qualified Data.Map.Strict          as Map
 import qualified Data.Map.Monoidal.Strict as MonoidalMap
 import           Data.Tuple.Strict
@@ -27,20 +27,50 @@ diffGroup = testGroup "PublicationPoints" [
         QC.testProperty "BGPSecPayload applyDiff works" $ checkDiff @(Set BGPSecPayload),
         QC.testProperty "Trace applyDiff works" $ checkDiff @(Set Trace),
 
-        QC.testProperty "Roas applyDiff works" $ \(before, after) -> let
-                noEmptyOnes = Roas . MonoidalMap.filter (not . MonoidalMap.null) . unRoas    
-            in checkDiff (noEmptyOnes before, noEmptyOnes after),
+        QC.testProperty "Roas applyDiff works" $ \(before :: Roas, after :: Roas) -> 
+            checkDiff before after,
 
-        QC.testProperty "Vrps applyDiff works" $ \(before, after) -> let
-                noEmptyOnes = Vrps . MonoidalMap.filter (not . Set.null) . unVrps    
-            in checkDiff (noEmptyOnes before, noEmptyOnes after),
+        QC.testProperty "Vrps applyDiff works" $ \(before :: Vrps, after :: Vrps) ->
+            checkDiff before after,
 
-        QC.testProperty "Validations applyDiff works" $ \(before, after) -> let
-                noEmptyOnes = Validations . Map.filter (not . Set.null) . unValidations    
-            in checkDiff (noEmptyOnes before, noEmptyOnes after)
+        QC.testProperty "Validations applyDiff works" $ \(before :: Validations, after :: Validations) -> 
+            checkDiff before after
     ]
 
 
-checkDiff :: (Eq a, Diffable a) => (a, a) -> Bool
-checkDiff (before, after) = 
-    applyDiff before (makeDiff before after) == after
+checkDiff :: (Eq a, Shrinkable a, Diffable a) => a -> a -> Bool
+checkDiff before after = 
+    if (applyDiff before (makeDiff before after) == after)
+        then True
+        else let 
+            bShrunk = shrink before
+            aShrunk = shrink after
+        in applyDiff bShrunk (makeDiff bShrunk aShrunk) == aShrunk        
+
+class Shrinkable a where 
+    shrink :: a -> a     
+    shrink = id
+    isEmpty :: a -> Bool
+    isEmpty _ = False
+
+instance Shrinkable (Set v)
+
+instance Shrinkable Roas where    
+    shrink = Roas . shrink . unRoas
+    isEmpty = isEmpty . unRoas
+
+instance Shrinkable Vrps where    
+    shrink = Vrps . shrink . unVrps
+    isEmpty = isEmpty . unVrps
+
+instance Shrinkable Validations where    
+    shrink = Validations . shrink . unValidations
+    isEmpty = isEmpty . unValidations
+
+instance Shrinkable v => Shrinkable (Map k v) where    
+    shrink = Map.filter (not . isEmpty) . Map.map shrink 
+    isEmpty = Map.null
+
+instance Shrinkable v => Shrinkable (MonoidalMap.MonoidalMap k v) where    
+    shrink = MonoidalMap.filter (not . isEmpty) . MonoidalMap.map shrink 
+    isEmpty = MonoidalMap.null
