63bc9263d0e3937eb21af43448cdd7a7017b45e8
Mikhail Puzanov <misha.puzanov@gmail.com>
2024-11-21 14:29:08 +0100

    Fix tests on macos

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cp package-template.yaml package.yaml
-stack test rpki-prover:lib rpki-prover:test:rpki-prover-test --stack-yaml=stack.yaml
+if [[ $(uname -m) == 'arm64' ]]; then
+    EXTRA_LIBRARIES="--extra-include-dirs=/opt/homebrew/include --extra-lib-dirs=/opt/homebrew/lib"
+fi
+stack test rpki-prover:lib rpki-prover:test:rpki-prover-test ${EXTRA_LIBRARIES}
