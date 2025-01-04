375be549ec6a717007653d38cdaad2c38a26455c
Misha Puzanov <misha.puzanov@gmail.com>
2025-01-04 18:54:27 +0100

    Fix merge artifacts

--- src/RPKI/UniqueId.hs
+++ src/RPKI/UniqueId.hs
@@ -8,8 +8,4 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
-<<<<<<< HEAD
-    "srcHash#f9716b22896a1e0871a0d396f5687e43c76e2d500ac38ff535f4142fbe7cd8ba#srcHash"
-=======
-    "srcHash#28d120fa1bedd86733bd920e1065f615a8dd4a963b4dd8f89cade64c77447ddb#srcHash"
->>>>>>> 163f9a3 (Manifest location and SIA mismatch is not an error)
+    "srcHash#10c449bc50c47a2f194e5c7749a7a3047facbedfd55d4c16f915bff1a99a0ea1#srcHash"
