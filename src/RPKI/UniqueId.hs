d1a8e1f84188eed8f049d76d204062d90f0425ff
Mikhail Puzanov <misha.puzanov@gmail.com>
2025-03-25 17:38:22 +0100

    Some vibe coding for trying unix sockets

--- src/RPKI/UniqueId.hs
+++ src/RPKI/UniqueId.hs
@@ -8,4 +8,4 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
-    "srcHash#791b795c16baf619a083461d5fb1f9c5de506245f80ef0907d7b33e0d3338a00#srcHash"
+    "srcHash#b9b663cd19b8077ba822517fb81f52cb9aeaa7a40d988eb345ab9623ae50b94e#srcHash"
