121b822ce11ab67993549664a82d94794e2118d6
Misha Puzanov <misha.puzanov@gmail.com>
2025-06-14 15:18:17 +0200

    Fix withValidityApi flag

--- src/RPKI/UniqueId.hs
+++ src/RPKI/UniqueId.hs
@@ -8,4 +8,4 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
-    "srcHash#a35240bde11ac5140014a99a1284e1f35a68ee3a4b0f342d8349db1457d44bfa#srcHash"
+    "srcHash#56dfdc1df3177b69473198d0ae5992e805cb9fe300b6c1b5b2a9238c447bfb75#srcHash"
