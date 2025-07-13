fda45de1e4a1abf0976c2f21def33c041d3c5e12
Mikhail Puzanov <misha.puzanov@gmail.com>
2025-07-13 18:17:40 +0200

    Make Instant an nanosecond Int64

--- src/RPKI/UniqueId.hs
+++ src/RPKI/UniqueId.hs
@@ -8,4 +8,4 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
-    "srcHash#270a7ffbbb37f8d0ebbeac110ba0f404ec6c4d21c2308cf461ea9bdfd2c641e2#srcHash"
+    "srcHash#a3371045a05f63c04877517a50f7223b120828a76da80e8b5c6121db5faf8898#srcHash"
