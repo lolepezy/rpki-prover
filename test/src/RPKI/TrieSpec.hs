8bf81cc657a560e055f8be69eb59dfe2cb0c6bd3
Mikhail Puzanov <misha.puzanov@gmail.com>
2025-09-11 09:53:54 +0200

    Make tests better

--- test/src/RPKI/TrieSpec.hs
+++ test/src/RPKI/TrieSpec.hs
@@ -57,23 +57,19 @@ trieSpec = testGroup "Trie" [
         prop_semigroup_associativity        
     ]
 
--- | Simple key type for testing
 newtype TestKey = TestKey Int
     deriving stock (Eq, Ord, Show, Generic)
     deriving newtype (QC.Arbitrary)
 
--- | Path of keys
 newtype KeyPath = KeyPath [TestKey]
     deriving stock (Eq, Show)
 
 instance QC.Arbitrary KeyPath where
     arbitrary = do
-        len <- choose (0, 5)  -- Keep paths reasonably short
+        len <- choose (0, 5)
         KeyPath <$> replicateM len QC.arbitrary
-    
     shrink (KeyPath ks) = KeyPath <$> QC.shrink ks
 
--- | List of key-value pairs
 newtype KeyValueList = KeyValueList [([TestKey], String)]
     deriving stock (Eq, Show)
 
@@ -84,57 +80,47 @@ instance QC.Arbitrary KeyValueList where
         values <- replicateM n genNonEmptyString
         return $ KeyValueList $ zip (map (\(KeyPath ks) -> ks) keys) values
       where
-        genNonEmptyString = listOf1 $ elements ['a'..'z']
-    
+        genNonEmptyString = listOf1 $ elements ['a'..'z']    
     shrink (KeyValueList kvs) = KeyValueList <$> QC.shrink kvs
 
 -- | Helper to build a trie from a list of key-value pairs
 kvListToTrie :: KeyValueList -> Trie TestKey String
 kvListToTrie (KeyValueList kvs) = fromList kvs
 
-prop_lookup_after_insert :: KeyPath -> String -> Bool
-prop_lookup_after_insert (KeyPath ks) v = 
-    Trie.lookup ks (insert ks v mempty) == Just v
+prop_lookup_after_insert :: KeyPath -> String -> KeyValueList -> Bool
+prop_lookup_after_insert (KeyPath ks) v kvl =
+    Trie.lookup ks (insert ks v (kvListToTrie kvl)) == Just v
 
--- | Property: lookup of non-existent key returns Nothing
 prop_lookup_nonexistent :: KeyPath -> KeyPath -> String -> QC.Property
 prop_lookup_nonexistent (KeyPath ks1) (KeyPath ks2) v =
     ks1 /= ks2 QC.==> isNothing (Trie.lookup ks2 (insert ks1 v mempty))
 
--- | Property: inserting with the same key overrides previous values
-prop_insert_override :: KeyPath -> String -> String -> Bool
-prop_insert_override (KeyPath ks) v1 v2 =
-    Trie.lookup ks (insert ks v2 (insert ks v1 mempty)) == Just v2
+prop_insert_override :: KeyPath -> String -> String -> KeyValueList -> Bool
+prop_insert_override (KeyPath ks) v1 v2 kvl =
+    Trie.lookup ks (insert ks v2 (insert ks v1 (kvListToTrie kvl))) == Just v2
 
--- | Property: delete removes keys
-prop_delete_removes :: KeyPath -> String -> Bool
-prop_delete_removes (KeyPath ks) v =
-    isNothing (Trie.lookup ks (delete ks (insert ks v mempty)))
+prop_delete_removes :: KeyPath -> String -> KeyValueList -> Bool
+prop_delete_removes (KeyPath ks) v kvl =
+    isNothing (Trie.lookup ks (delete ks (insert ks v (kvListToTrie kvl))))
 
--- | Property: trie is null after deleting the only key
 prop_null_after_delete :: KeyPath -> String -> Bool
 prop_null_after_delete (KeyPath ks) v =
     Trie.null (delete ks (insert ks v mempty))
 
--- | Property: size counts the number of key-value pairs correctly
 prop_size_counts_correctly :: KeyValueList -> Bool
 prop_size_counts_correctly kvl@(KeyValueList kvs) =
     size (kvListToTrie kvl) == length (nub (map fst kvs))
 
-
--- | Property: mempty is the left identity for (<>)
 prop_monoid_left_identity :: KeyValueList -> Bool
 prop_monoid_left_identity kvl =
     let t = kvListToTrie kvl
     in mempty <> t == t
 
--- | Property: mempty is the right identity for (<>)
 prop_monoid_right_identity :: KeyValueList -> Bool
 prop_monoid_right_identity kvl =
     let t = kvListToTrie kvl
     in t <> mempty == t
 
--- | Property: (<>) is associative
 prop_semigroup_associativity :: KeyValueList -> KeyValueList -> KeyValueList -> Bool
 prop_semigroup_associativity kvl1 kvl2 kvl3 =
     let t1 = kvListToTrie kvl1
