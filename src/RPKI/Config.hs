3fc7b523abb68f08c3aec46e356f61ecda855c47
Mikhail Puzanov <misha.puzanov@gmail.com>
2025-12-29 01:19:46 +0100

    ADjust

--- src/RPKI/Config.hs
+++ src/RPKI/Config.hs
@@ -306,7 +306,7 @@ adjustConfig config = config
 
 adjustWorkerConfig :: Config -> Timebox -> Config
 adjustWorkerConfig config (Timebox timeout) = config
-        -- There's no point in having transaction timeout
+        -- There's no point in having RW-transaction timeout
         -- longer than the worker timeout
         & #storageConfig . #rwTransactionTimeout %~ (`min` (timeout - Seconds 1))
 
