d887d7e1228c9cd1cca878297b865346e6322d3c
Mikhail Puzanov <misha.puzanov@gmail.com>
2026-01-14 00:14:18 +0100

    Try different static builder

--- src/RPKI/UniqueId.hs
+++ src/RPKI/UniqueId.hs
@@ -9,4 +9,4 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
-    "srcHash#2a57008109385c3405c4fc83886539dd1d940498ba3257306c226e237dfd4682#srcHash"
+    "srcHash#f499f90e7e8eeb03a1b913ce361052a2f60c0c1000339ba6bc0ecd56399b89bf#srcHash"
