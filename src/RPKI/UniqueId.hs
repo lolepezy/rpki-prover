f0bb9ef448e909d991a68d88a5cb4be324c45360
Misha Puzanov <misha.puzanov@gmail.com>
2026-01-14 00:30:41 +0100

    Bump id

--- src/RPKI/UniqueId.hs
+++ src/RPKI/UniqueId.hs
@@ -9,4 +9,4 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
-    "srcHash#f499f90e7e8eeb03a1b913ce361052a2f60c0c1000339ba6bc0ecd56399b89bf#srcHash"
+    "srcHash#50214cd3d115aa68ac432167b5ee57d86a08938cc263723fdfaa7b50ec501b5e#srcHash"
