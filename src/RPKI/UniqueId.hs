e27cd258b36ec515e455e29f38399fe06d267b00
Misha Puzanov <misha.puzanov@gmail.com>
2026-02-21 13:23:30 +0000

    Use aki for referring to CAs

--- src/RPKI/UniqueId.hs
+++ src/RPKI/UniqueId.hs
@@ -9,4 +9,9 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
+<<<<<<< HEAD
     "srcHash#9b83137f4b6a8d97c1e4991e8e3b7e047f5cd0c88feef14ca6bfe2cb21ebe635#srcHash"
+=======
+    "srcHash#172f3fca97369e42da55e21ae06f138370d9a671d58dbf159c128c0760236f4f#srcHash"
+    
+>>>>>>> 85581e81 (Use aki for referring to CAs)
