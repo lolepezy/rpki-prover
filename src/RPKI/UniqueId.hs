09fb652c73320223f9a9cd5417899e0dc2cee7f3
Misha Puzanov <misha.puzanov@gmail.com>
2026-02-22 22:22:44 +0000

    Fix some warnings

--- src/RPKI/UniqueId.hs
+++ src/RPKI/UniqueId.hs
@@ -9,5 +9,5 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
-    "srcHash#03277a159a9d379a8474ebb4d2e4eaa13036f9baceca7fb14264f3bdd73a78f3#srcHash"
+    "srcHash#58031adf4471fd3d3930f6c37964cfaef4e778884cf03d0ea42d93df071c3ac0#srcHash"
     
