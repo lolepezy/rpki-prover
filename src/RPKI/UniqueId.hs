09f8f64f86d5870821105c07c1ec00abd83d7ffc
Mikhail Puzanov <misha.puzanov@gmail.com>
2026-03-05 09:28:37 +0100

    Implement expireObjects

--- src/RPKI/UniqueId.hs
+++ src/RPKI/UniqueId.hs
@@ -11,6 +11,7 @@ thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <>
     -- that calculates hash of the source tree and configuration/build files     
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     "srcHash#9b83137f4b6a8d97c1e4991e8e3b7e047f5cd0c88feef14ca6bfe2cb21ebe635#srcHash"
 =======
     "srcHash#172f3fca97369e42da55e21ae06f138370d9a671d58dbf159c128c0760236f4f#srcHash"
@@ -20,3 +21,7 @@ thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <>
     "srcHash#72999d3eb12c7e8542e94496a4dff5abbaa6526aa2e378ec49c4ee8bafc04d7a#srcHash"
     
 >>>>>>> 0c582057 (Start implementing expireObjects)
+=======
+    "srcHash#a7ec53516b67b3e07b87a4eb0c78beb56f0a0170ea260ac2795d98b656ecc19f#srcHash"
+    
+>>>>>>> f2e5a134 (Implement expireObjects)
