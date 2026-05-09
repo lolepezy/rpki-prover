219c71b466977409e487b65bdf08d8563bfc0763
Mikhail Puzanov <mpuzanov@ripe.net>
2026-05-09 23:25:40 +0200

    Adjust unique id

--- src/RPKI/UniqueId.hs
+++ src/RPKI/UniqueId.hs
@@ -9,4 +9,4 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
-    "srcHash#9b83137f4b6a8d97c1e4991e8e3b7e047f5cd0c88feef14ca6bfe2cb21ebe635#srcHash"
+    "srcHash#d3e58845e93d2a3f4b8ba8149be6e12fa984d6fa191571da7ba95ecaf4af2dbb#srcHash"
