8483c536736b66609c13bb6b9c2e59a144cbf700
Misha Puzanov <misha.puzanov@gmail.com>
2026-09-12 00:26:01 +0100

    Remove another sanctimonious comment

--- src/RPKI/Meta/UniqueId.hs
+++ src/RPKI/Meta/UniqueId.hs
@@ -9,4 +9,4 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
-    "srcHash#6b4878064d9a2211b33b4c5c693281c22b6a6731b3fbf0ba7e5b82ba065f4cdf#srcHash"        
+    "srcHash#f40544e84d1853a7c2f6b6198989789da2efc1bf5834cff1037ea0ef74a076d6#srcHash"        
