f77dc226a6ee78f8856c0a52b0aa4f60be1396f6
Misha Puzanov <misha.puzanov@gmail.com>
2026-09-11 23:44:39 +0100

    Warn about approaching the limits of some resources

--- src/RPKI/Meta/UniqueId.hs
+++ src/RPKI/Meta/UniqueId.hs
@@ -9,4 +9,4 @@ thisExecutableVersion :: ExecutableVersion
 thisExecutableVersion = ExecutableVersion $ rpkiProverVersion <> " " <> 
     -- The content is to be updated by the 'src-hash' script 
     -- that calculates hash of the source tree and configuration/build files     
-    "srcHash#dd19ceac998fd1df388ffbf3653b83295a170f6020c23da9d5305bc74e564bd3#srcHash"        
+    "srcHash#5935d7753e05a8636aeb800da6c1f68a29be580c5b0223bc40db249fc566e61a#srcHash"        
